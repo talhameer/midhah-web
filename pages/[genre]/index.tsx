b7a5e21247aea95dd08e1bf62356c47beb9fab7d
Talha Meer <muhammad.talha@consoliads.com>
2023-04-06 23:01:17 +0500

    make the listing page to client side & add infinite scrool

---
+++ pages/[genre]/index.tsx
@@ -0,0 +1,98 @@
+import Lyrics from "@/models/Lyrics";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect, useRef, useState } from "react";
+
+export default function GenreListPage() {
+  const router = useRouter();
+  const genre = router.query.genre as string;
+
+  const [lyrics, setLyrics] = useState<Lyrics[]>([]);
+  const [page, setPage] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
+  const [hasMoreData, setHasMoreData] = useState(true);
+
+  const lastLyricRef = useRef<HTMLLIElement>(null);
+
+  useEffect(() => {
+    const options = {
+      root: null,
+      rootMargin: "20px",
+      threshold: 0,
+    };
+
+    const lastLyricNode = lastLyricRef.current;
+
+    const observer = new IntersectionObserver(([entry]) => {
+      if (entry.isIntersecting && !isLoading) {
+        setPage((prevPage) => prevPage + 1);
+      }
+    }, options);
+
+    if (lastLyricNode) {
+      observer.observe(lastLyricNode);
+    }
+
+    return () => {
+      if (lastLyricNode) {
+        observer.unobserve(lastLyricNode);
+      }
+    };
+  }, [isLoading, page, lastLyricRef]);
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    if (genre && hasMoreData) {
+      fetch(`http://api.midhah.com/v2/lyrics/${genre}?page=${page}&size=100`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => {
+          if (!response.ok) {
+            setHasMoreData(false);
+          }
+          return response.json();
+        })
+        .then((res) => {
+          if (res.data) {
+            setLyrics((prevLyrics) => [...prevLyrics, ...res.data]);
+          }
+          setIsLoading(false);
+        });
+    }
+  }, [genre, hasMoreData, page]);
+
+  return (
+    <div className="container mx-auto w-full md:w-[85%]">
+      <div className="relative overflow-hidden naat card mb-5 md:rounded-[10px]">
+        <div className="py-[60px] md:py-[150px] text-center">
+          <h1 className="text-2xl md:text-5xl mb-1 text-white">
+            NAAT E RASOOL
+          </h1>
+        </div>
+      </div>
+      <ul className="md:grid md:grid-cols-2 w-full">
+        {lyrics.map((lyric: Lyrics, index: number) => (
+          <Link href={`/${genre}/${lyric.slug}`} key={lyric.slug}>
+            <li
+              className="flex flex-row my-1"
+              ref={index === lyrics.length - 1 ? lastLyricRef : undefined}
+            >
+              <div className="select-none cursor-pointer hover:bg-gray-50 flex flex-1 items-center p-4">
+                <div className="flex-1 pl-1 mr-16">
+                  <div className="text-gray-600  ">{lyric.title}</div>
+                  <span className="text-gray-400  text-sm uppercase">
+                    {lyric.genre}
+                  </span>
+                </div>
+              </div>
+            </li>
+          </Link>
+        ))}
+      </ul>
+    </div>
+  );
+}
